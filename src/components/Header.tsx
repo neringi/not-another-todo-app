bde416be0e7380fbebc420ed6b93e1026b94d5d3
Neringa Chomanskyte <chomanskyten@gmail.com>
2025-05-01 22:21:32 +0100

    basic setup of main components

---
+++ src/components/Header.tsx
@@ -0,0 +1,5 @@
+export default function Header() {
+  return (
+    <header className="col-[1/3] row[1/2] bg-[#fbf5ed] border-b border-black/[0.08]"></header>
+  )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,5 @@
+export default function Sidebar() {
+  return (
+    <section className="col-[2/3] row-[2/3] bg-[#fffcf9] border-l border-black/[0.08]"></section>
+  )
+}
