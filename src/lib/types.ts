ceeb61217694e930e11e7d5259f5640797383403
Neringa Chomanskyte <chomanskyten@gmail.com>
2025-05-05 11:56:02 +0100

    fix types, remove garbage

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,26 +1,5 @@
-export type AddTodoFormProps = {
-    handleAddTodo: (todoText: string) => void;
-};
-
 export type Todo = {
   id: number,
   text: string;
   isCompleted: boolean;
-};
-
-export type ButtonProps = {
-    buttonType?: 'primary' | 'secondary'; 
-    children: React.ReactNode;
-};
-
-export type CounterProps = {
-    totalTodos: number;
-    completedTodos: number;
-};
-
-
-
-export type HeaderProps = {
-    totalTodos: number;
-    completedTodos: number;
 };
